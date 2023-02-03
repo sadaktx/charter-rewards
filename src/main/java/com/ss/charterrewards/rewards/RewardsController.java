654254405f34b1ffd4413a8e66b406d0814974f5
sadaktx <sadak.shaik@gmail.com>
2023-02-03 00:09:51 -0600

    Update java docs

--- src/main/java/com/ss/charterrewards/rewards/RewardsController.java
+++ src/main/java/com/ss/charterrewards/rewards/RewardsController.java
@@ -17,7 +17,7 @@ import com.ss.charterrewards.entity.CustomerTransaction;
 
 /**
 * RewardsController has all the CRUD REST end points for the clients to access
-* customer and their rewards information
+* customer and their rewards information. All responses are returned in JSON.
 * 
 * @author Sadak Shaik
 * 
@@ -29,26 +29,70 @@ public class RewardsController {
 	@Autowired
 	private RewardsService rewardsService;
 	
+	/**
+	* Handles GET requests to /customers endpoint and returns all the customers in the application.  
+	* 
+	* @return all the customers in the application
+	*/
 	@GetMapping(value = "/customers")
 	public List<Customer>getCustomerAll() {
 		return rewardsService.getCustomerAll();
 	}
+	
+	/**
+	* Handles GET requests to /customer/{id} endpoint and returns the customer with {id}.  
+	* 
+	* @param id the customer id
+	* @return all the customers in the application
+	*/
 	@GetMapping(value = "/customer/{id}")
 	public Optional<Customer>getCustomer(@PathVariable("id") Integer id) {
 		return rewardsService.getCustomerById(id);
 	}
+	
+	/**
+	* Handles POST requests to /customers/add endpoint and adds the customer(s).  
+	* 
+	* @param customers the list of customers to be added in JSON
+	* @return all the customers in the application
+	*/
 	@PostMapping("/customers/add")
 	public List<Customer> addCustomers(@RequestBody List<Customer> customers) {
 		return rewardsService.addCustomers(customers);
 	}
+	
+	/**
+	* Handles PUT requests to /customer/{id} endpoint and updates the customer with {id}.  
+	* 
+	* @param customer the customer to be updated in JSON
+	* @param id the customer id 
+	* @return all the customers in the application
+	*/
 	@PutMapping("/customer/{id}")
 	public Customer updateCustomer(@RequestBody Customer customer, @PathVariable Integer id) {
 		return rewardsService.updateCustomer(customer, id);
 	}
+	
+	/**
+	* Handles POST requests to /customer/{customerId}/transactions/add endpoint 
+	* and adds the customer transactions(s) to the customer with {customerId}  
+	* 
+	* @param customerTransactions the list of customerTransactions to be added in JSON
+	* @param customerId the customer id 
+	* @return all the customerTransactions 
+	*/
 	@PostMapping("/customer/{customerId}/transactions/add")
 	public List<CustomerTransaction> addCustomerTransactions(@RequestBody List<CustomerTransaction> customerTransactions, @PathVariable Integer customerId) {
 		return rewardsService.addCustomerTransactions(customerTransactions, customerId);
 	}
+	
+	/**
+	* Handles PUT requests to /customer/transaction/{id} endpoint and updates the customer transaction with {id}.  
+	* 
+	* @param customerTransaction the customer transaction to be updated in JSON
+	* @param id the customer transaction id 
+	* @return the updated customer transaction
+	*/
 	@PutMapping("/customer/transaction/{id}")
 	public CustomerTransaction updateCustomerTransaction(@RequestBody CustomerTransaction customerTransaction, @PathVariable Integer id) {
 		return rewardsService.updateCustomerTransaction(customerTransaction, id);
